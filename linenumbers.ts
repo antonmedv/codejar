245e213622ef60574ae76d9b7969b43249c592f5
Yurii Kadirov <admin@sirkadirov.com>
2020-07-10 15:23:33 +0300

    Improve lines counting + allow overflow

--- linenumbers.ts
+++ linenumbers.ts
@@ -14,12 +14,13 @@ export function withLineNumbers(
     class: "codejar-linenumbers",
     wrapClass: "codejar-wrap",
     width: "35px",
-    backgroundColor: "rgba(128, 128, 128, 0.15)",
+    backgroundColor: "rgba(128, 128, 128, 0)",
     color: "",
     ...options
   }
 
   let lineNumbers: HTMLElement
+
   return function (editor: HTMLElement) {
     highlight(editor)
 
@@ -28,11 +29,11 @@ export function withLineNumbers(
     }
 
     const code = editor.textContent || ""
-    const linesCount = code.replace(/\n+$/, "\n").split("\n").length + 1
+    const linesCount = code.split(/\r\n|\r|\n/).length + (code.endsWith('\r') || code.endsWith('\n') ? 0 : 1)
 
     let text = ""
     for (let i = 1; i < linesCount; i++) {
-      text += `${i}\n`
+      text += `${i}\r\n`
     }
 
     lineNumbers.innerText = text
@@ -45,6 +46,9 @@ function init(editor: HTMLElement, opts: Options): HTMLElement {
   const wrap = document.createElement("div")
   wrap.className = opts.wrapClass
   wrap.style.position = "relative"
+  wrap.style.setProperty("overflow-x", "auto")
+  wrap.style.setProperty("overflow-y", "auto")
+  wrap.style.setProperty("resize", "vertical")
 
   const lineNumbers = document.createElement("div")
   lineNumbers.className = opts.class
@@ -56,10 +60,11 @@ function init(editor: HTMLElement, opts: Options): HTMLElement {
   lineNumbers.style.left = "0px"
   lineNumbers.style.bottom = "0px"
   lineNumbers.style.width = opts.width
-  lineNumbers.style.overflow = "hidden"
+  lineNumbers.style.overflow = "unset"
   lineNumbers.style.backgroundColor = opts.backgroundColor
   lineNumbers.style.color = opts.color || css.color
   lineNumbers.style.setProperty("mix-blend-mode", "difference")
+  lineNumbers.style.setProperty("user-select", "none")
 
   // Copy editor styles
   lineNumbers.style.fontFamily = css.fontFamily
@@ -73,9 +78,14 @@ function init(editor: HTMLElement, opts: Options): HTMLElement {
   // Tweak editor styles
   editor.style.paddingLeft = `calc(${opts.width} + ${lineNumbers.style.paddingLeft})`
   editor.style.whiteSpace = "pre"
+  editor.style.setProperty("overflow-x", "unset")
+  editor.style.setProperty("overflow-y", "unset")
+  editor.style.setProperty("resize", "none")
+  editor.style.setProperty("min-height", "100%")
 
   // Swap editor with a wrap
   editor.parentNode!.insertBefore(wrap, editor)
   wrap.appendChild(editor)
+  
   return lineNumbers
 }
