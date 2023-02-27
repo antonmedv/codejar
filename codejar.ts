f386c07a0ef3dbca89da645c01f09514857406e1
Gnietschow <tobias.tenbusch@googlemail.com>
2023-02-27 22:29:45 +0100

    Added fix for Strg+C in Firefox (#89)
    * bugfix Strg+C in Firefox sometimes changing selection
    
    * added missing param type

--- codejar.ts
+++ codejar.ts
@@ -104,7 +104,7 @@ export function CodeJar(editor: HTMLElement, highlight: (e: HTMLElement, pos?: P
       }
     }
 
-    if (isLegacy) restore(save())
+    if (isLegacy && !isCopy(event)) restore(save())
   })
 
   on('keyup', event => {
@@ -461,6 +461,10 @@ export function CodeJar(editor: HTMLElement, highlight: (e: HTMLElement, pos?: P
     let key = event.key || event.keyCode || event.which
     if (!key) return undefined
     return (typeof key === 'string' ? key : String.fromCharCode(key)).toUpperCase()
+  }
+  
+  function isCopy(event: KeyboardEvent) {
+      return isCtrl(event) && event.code === 'KeyC';
   }
 
   function insert(text: string) {
